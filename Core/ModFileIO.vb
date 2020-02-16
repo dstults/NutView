63363f677ea717b5c57f20988191a0468cc24e74
Darren <drankof@gmail.com>
2020-02-15 17:52:00 -0800

    Initial Mods to Detect Imported Data Program Type

--- Core/ModFileIO.vb
+++ Core/ModFileIO.vb
@@ -1,61 +1,34 @@
-﻿Module ModFileIO
+﻿Partial Module ModFileIO
+
+    Private Enum IMode
+        Unset
+        NutCheck
+        AdvancedIPScanner
+        NetCat
+        Metasploit
+    End Enum
 
     Public Sub ParseFile(FilePath As String)
         Dim FileInput() As String = IO.File.ReadAllLines(FilePath)
         Dim InputTime As DateTime = DateTime.Now
+        Dim InputMode As Integer = IMode.NutCheck
         For Each iLine As String In FileInput
             Dim iPart() As String = Split(iLine, ",")
-            Select Case LCase(iPart(0))
-                Case "test time"
-                    InputTime = CDate(iPart(1))
-                Case "input ips"
-                    ' IGNORE - ALREADY INCLUDED
-                Case "input ports"
-                    ' IGNORE - ALREADY INCLUDED
-                Case "timeout"
-                    ' IGNORE FOR NOW
-                Case "tests"
-                    ' IGNORE - ALREADY INCLUDED
+            Select Case iPart(0)
+                Case "TEST TIME", "INPUT IPS", "INPUT PORTS", "TIMEOUT", "TESTS"
+                    InputMode = IMode.NutCheck
                 Case Else
-                    Dim aHost As ClsHost = AllHosts.Find(Function(p) p.IP = iPart(0))
-                    'If AllHosts.Count > 0 Then aHost
-                    If aHost Is Nothing Then
-                        aHost = New ClsHost
-                        AllHosts.Add(aHost)
-                        aHost.IP = iPart(0)
-                    End If
-                    Dim tcpMode As Boolean = False
-                    Dim udpMode As Boolean = False
-                    Dim intA As Integer = 1
-                    Do Until intA >= iPart.Count
-                        Select Case iPart(intA)
-                            Case "+ping", "ping hit"
-                                aHost.Ping.Add(True, InputTime)
-                            Case "-ping", "ping miss"
-                                aHost.Ping.Add(False, InputTime)
-                            Case "tcp"
-                                tcpMode = True
-                                udpMode = False
-                            Case "udp"
-                                tcpMode = False
-                                udpMode = True
-                            Case Else
-                                If tcpMode Then
-                                    Select Case Left(iPart(intA), 1)
-                                        Case "+"
-                                            Dim aPort As Integer = CInt(Mid(iPart(intA), 2))
-                                            aHost.Tcp.Add(True, aPort, InputTime)
-                                        Case "-"
-                                            Dim aPort As Integer = CInt(Mid(iPart(intA), 2))
-                                            aHost.Tcp.Add(False, aPort, InputTime)
-                                        Case Else
-                                            ' DO NOTHING
-                                    End Select
-                                End If
-                        End Select
-                        aHost.GetEmptiness()
-                        intA += 1
-                    Loop
+                    If Net.iPart(0) Then
+            End Select
+        Next
+        For Each iLine As String In FileInput
+            Select Case InputMode
+                Case IMode.NutCheck
+                    Dim iPart() As String = Split(iLine, ",")
+                    NutCheckImport(FileInput)
+                Case IMode.AdvancedIPScanner
+                Case IMode.NetCat
+                Case IMode.Metasploit
             End Select
         Next
         ' Optimization is good!
---
+++ Core/ModFileIO_NutCheck.vb
@@ -0,0 +1,58 @@
+﻿Partial Module ModFileIO
+
+    Private Sub NutCheckImport(iPart() As String)
+        Select Case LCase(iPart(0))
+            Case "test time"
+                InputTime = CDate(iPart(1))
+            Case "input ips"
+                    ' IGNORE - ALREADY INCLUDED
+            Case "input ports"
+                    ' IGNORE - ALREADY INCLUDED
+            Case "timeout"
+                    ' IGNORE FOR NOW
+            Case "tests"
+                ' IGNORE - ALREADY INCLUDED
+            Case Else
+                Dim aHost As ClsHost = AllHosts.Find(Function(p) p.IP = iPart(0))
+                'If AllHosts.Count > 0 Then aHost
+                If aHost Is Nothing Then
+                    aHost = New ClsHost
+                    AllHosts.Add(aHost)
+                    aHost.IP = iPart(0)
+                End If
+                Dim tcpMode As Boolean = False
+                Dim udpMode As Boolean = False
+                Dim intA As Integer = 1
+                Do Until intA >= iPart.Count
+                    Select Case iPart(intA)
+                        Case "+ping", "ping hit"
+                            aHost.Ping.Add(True, InputTime)
+                        Case "-ping", "ping miss"
+                            aHost.Ping.Add(False, InputTime)
+                        Case "tcp"
+                            tcpMode = True
+                            udpMode = False
+                        Case "udp"
+                            tcpMode = False
+                            udpMode = True
+                        Case Else
+                            If tcpMode Then
+                                Select Case Left(iPart(intA), 1)
+                                    Case "+"
+                                        Dim aPort As Integer = CInt(Mid(iPart(intA), 2))
+                                        aHost.Tcp.Add(True, aPort, InputTime)
+                                    Case "-"
+                                        Dim aPort As Integer = CInt(Mid(iPart(intA), 2))
+                                        aHost.Tcp.Add(False, aPort, InputTime)
+                                    Case Else
+                                        ' DO NOTHING
+                                End Select
+                            End If
+                    End Select
+                    aHost.GetEmptiness()
+                    intA += 1
+                Loop
+        End Select
+    End Sub
+
+End Module
