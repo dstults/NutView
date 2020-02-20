86d7965bddf8cf55fc40ed864db9d64d2011aff2
Darren <drankof@gmail.com>
2020-02-20 11:23:52 -0800

    Version Bump

--- Core/ModCore.vb
+++ Core/ModCore.vb
@@ -3,7 +3,7 @@
 
 Module ModCore
 
-    Public ProgVersion As String = "v0.55"
+    Public ProgVersion As String = "v0.59"
     Public MainWindow As New FormNutView
 
     Public AllHosts As New List(Of ClsHost)
